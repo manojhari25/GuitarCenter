fd8e035a92f5010efec322405cfc0e6e59648cde
manojhari25 <harimanojui@gmail.com>
2017-07-28 09:57:11 -0700

    Sample Guitar Center Code

---
+++ guitarcenter.js
@@ -0,0 +1,34 @@
+function clock(){
+
+    document.getElementById("chatbox").style.display="block";
+}
+
+function hiddenDiv(){
+
+    document.getElementById("chatbox").style.display = "none";
+    console.log("hello");
+}
+function git() {
+    document.getElementById("subnaving").style.display="block";
+
+}
+function hiding() {
+    document.getElementById("subnaving").style.display="none";
+
+}
+function base() {
+    document.getElementById("subnaving").style.display="block";
+
+}
+function hiding() {
+    document.getElementById("subnaving").style.display = "none";
+
+}
+function amps() {
+    document.getElementById("subnaving").style.display="block";
+
+}
+function hiding() {
+    document.getElementById("subnaving").style.display = "none";
+
+}
